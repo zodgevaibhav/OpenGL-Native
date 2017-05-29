5452c0edab271974d6d0c2a52f87a776d5ce00dc
zodgevaibhav <zodgevaibhav@gmail.com>
2017-05-29 20:26:47 +0530

    Commit changes

---
+++ Assignment1-BlankBlueWindow/Assignment1-BlankBlueWindow/BlankBlueWindow.cpp
@@ -0,0 +1,12 @@
+#include<Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hInstancePrev, LPSTR lpszcmdLine, int nCmdShow)
+{
+	WNDCLASSEX endclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+
+}
